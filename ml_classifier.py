191cef8cc8bcf39e2e8befafc6d412d1f16f878c
yago <yago.illing@googlemail.com>
2024-01-19 18:17:58 +0100

    add enemble classifier

---
+++ ml_classifier.py
@@ -0,0 +1,67 @@
+from tensorflow.keras.datasets import mnist
+from sklearn.metrics import accuracy_score
+from numpy.core.arrayprint import format_float_scientific
+from sklearn import svm
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import BaggingClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import VotingClassifier, ExtraTreesClassifier
+from sklearn.ensemble import HistGradientBoostingClassifier
+import numpy as np
+
+# load dataset
+(trainX, trainy), (testX, testy) = mnist.load_data()
+
+clf1 = BaggingClassifier(svm.SVC(kernel='rbf', verbose=True, probability=False), max_samples=0.1)
+clf2 = RandomForestClassifier()
+clf3 = AdaBoostClassifier()
+clf4 = DecisionTreeClassifier()
+
+#
+clf5 = et_clf = ExtraTreesClassifier()
+clf6 = HistGradientBoostingClassifier()
+
+hard_voting_ensemble = VotingClassifier(estimators=[
+    ("random_forest", clf2),
+    # ("ada_boost", clf3),
+	# ("decision tree", clf4)
+	("HistGradientBoosting", clf6),
+	("extraTrees", clf5),
+    ("svm", clf1)
+
+], voting="hard", verbose=True)
+
+#classifiers = [clf1, clf2, clf3, clf4]
+classifiers = [clf2]
+
+#print(trainX.shape)
+#print(trainX[0].reshape(1, -1))
+
+reshaped_trainX = []
+reshaped_testX = []
+
+for x in trainX:
+	reshaped_trainX.append(np.array(x).reshape(1, -1)[0])
+
+for x in testX:
+	reshaped_testX.append(np.array(x).reshape(1, -1)[0])
+
+hard_voting_ensemble.fit(reshaped_trainX, trainy)
+
+# for idx, classifier in enumerate(classifiers):
+#   classifier.fit(reshaped_trainX, trainy)
+
+# predicted = []
+
+# for x, y in zip(testX, testy):
+# 	predictions = []
+# 	for classifier in classifiers:
+# 		prediction = classifier.predict(np.array(x).reshape(1, -1))
+# 		predictions.append(prediction[0])
+
+# 	predicted.append(max(set(predictions), key=predictions.count))
+
+predicted = hard_voting_ensemble.predict(reshaped_testX)
+assert len(predicted) == len(testy)
+print(accuracy_score(predicted, testy))
--- rulebased_classifier.py
+++ rulebased_classifier.py
@@ -10,32 +10,21 @@ import numpy as np
 reference_images = [[],[],[],[],[],[],[],[],[],[]]
 mean_reference_images = []
 
+#print(len(train), len(testy))
+
 # sort images by label
 for idx, i in enumerate(trainy):
 	reference_images[i].append(trainX[idx])
 
 for i in range(10):
- # define subplot
- # plt.subplot(330 + 1 + i)
- # plot raw pixel data
- #plt.imshow(np.average(refernces_images[i], axis=0), cmap=plt.get_cmap('gray'))
- mean_reference_images.append(np.average(reference_images[i], axis=0))
-
-for i in range(10):
- # define subplot
- plt.subplot(4,3,i+1)
- # plot raw pixel data
- plt.imshow(mean_reference_images[i], cmap=plt.get_cmap('gray'))
+	# define subplot
+	mean_reference_images.append(np.average(reference_images[i], axis=0))
+	plt.subplot(4,3,i+1)
+	# plot raw pixel data
+	plt.imshow(mean_reference_images[i], cmap=plt.get_cmap('gray'))
+ 
 
 predicted = []
-
-difflist = []
-for i in mean_reference_images:
-	difflist.append(np.sum( np.abs(trainX[4] - i) ))
-
-print(np.argmin(difflist), trainy[4])
-
-
 for x in testX:
 	difflist = []
 	for i in mean_reference_images:
